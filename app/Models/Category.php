1f2d30aefbf3bdc308fa8c46c1f0e3d99bdc835b
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2023-09-17 08:02:52 -0500

    mejoras en la relacion entre "category" y "products"

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -38,7 +38,7 @@ class Category extends Model
     |--------------------------------------------------------------------------
     */
     
-    public function product()
+    public function products()
     {
         return $this->hasMany(Product::class);
     }
---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
+//use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function test_has_many_products()
+    {
+        $category=new Category();
+        $this->assertInstanceOf(Collection::class,$category->products);
+    }
+}
