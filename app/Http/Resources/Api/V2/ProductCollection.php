961f356a6ceaa46433e7c2d2d09a71d11c05ae2f
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-09-22 10:44:58 -0500

    reorganizacion de los recursos

---
+++ app/Http/Resources/Api/V2/ProductCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Api\V2;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    public $collects = ProductResource::class;
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                "api_version" => "v1",
+                "content_type" => "JSON",
+                "organization" => "jcomte23",               
+                "response_description" => "List of products",
+                'authors' => [
+                    [
+                        "name" => "Javier Cómbita Téllez",
+                        "email" => "jcomte23@outlook.com",
+                        "homepage" => "https://javiercombita.com",
+                        "role" => "Lead Developer"
+                    ]
+                ],
+            ],
+        ];
+    }
+}
---
+++ app/Http/Resources/Api/V2/ProductResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Api\V2;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        $category = $this->category ? $this->category->name : null; 
+
+        return [
+            'id' => (int) $this->id,
+            'name' => (string) strtolower($this->name),
+            'price' => (float) $this->price,
+            'category' => (string) $category,
+            'creationDate' => (string) $this->created_at,
+            'lastUpdated' => (string) $this->updated_at
+        ];
+    }
+}
