86453d68b9f0cb72a78d8f4fa9e056ac7491f9c8
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-09-22 16:45:02 -0500

    validacion del scope con su test

--- tests/Unit/Models/ProductTest.php
+++ tests/Unit/Models/ProductTest.php
@@ -13,7 +13,23 @@ class ProductTest extends TestCase
 
     public function test_belongs_to_category()
     {
-        $product=Product::factory()->create();
-        $this->assertInstanceOf(Category::class,$product->category);
+        $product = Product::factory()->create();
+        $this->assertInstanceOf(Category::class, $product->category);
+    }
+
+    public function test_it_lists_the_first_1000_products_with_category_id_not_null()
+    {
+        // Crear 15 productos, 10 con category_id y 5 sin category_id
+        Product::factory(10)->create(['category_id' => 1]);
+        Product::factory(5)->create(['category_id' => null]);
+
+        // Usar el scope para obtener los primeros 10 productos con category_id
+        $productos = Product::listTheFirst1000Products()->get();
+
+        // Asegurarse de que se devuelvan solo 10 productos con category_id
+        $this->assertCount(10, $productos);
+
+        // Asegurarse de que todos los productos tengan category_id
+        $this->assertTrue($productos->every(fn ($producto) => $producto->category_id !== null));
     }
 }
