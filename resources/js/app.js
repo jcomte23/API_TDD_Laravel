0a61cc8898d08010fec19b3d944bcfe452a9eec4
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-09-24 21:43:28 -0500

    implementacion del boton para cambio de tema

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
 import './bootstrap';
+
+document.getElementById('modoDarkBtn').addEventListener('click', function () {
+    const html = document.querySelector('html');
+    const icon = document.querySelector('#modoDarkBtn i'); // Seleccione el ícono dentro del botón
+
+    if (html.classList.contains('dark')) {
+        // Cambiar a modo claro
+        html.classList.remove('dark');
+        localStorage.setItem('modo', 'light');
+        icon.classList.remove('bi-sun-fill'); // Eliminar la clase del ícono del modo oscuro
+        icon.classList.add('bi-moon-stars-fill'); // Agregar la clase del ícono del modo claro
+    } else {
+        // Cambiar a modo oscuro
+        html.classList.add('dark');
+        localStorage.setItem('modo', 'dark');
+        icon.classList.remove('bi-moon-stars-fill'); // Eliminar la clase del ícono del modo claro
+        icon.classList.add('bi-sun-fill'); // Agregar la clase del ícono del modo oscuro
+    }
+});
+
+// Verificar y cargar el modo preferido desde el almacenamiento local
+const modoAlmacenado = localStorage.getItem('modo');
+const html = document.querySelector('html');
+const icon = document.querySelector('#modoDarkBtn i'); // Seleccione el ícono dentro del botón
+
+if (modoAlmacenado === 'dark') {
+    html.classList.add('dark');
+    icon.classList.remove('bi-moon-stars-fill'); // Eliminar la clase del ícono del modo claro
+    icon.classList.add('bi-sun-fill'); // Agregar la clase del ícono del modo oscuro
+}
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -27,6 +27,10 @@
 </head>
 
 <body>
+    <button class="fixed bottom-4 right-4 p-2 px-3 rounded-full bg-gray-800 dark:bg-white text-white dark:text-black"
+        id="modoDarkBtn" style="z-index: 9999;"><i class="bi bi-moon-stars-fill text-xl"></i>
+    </button>
+
     <div class="font-sans text-gray-900 dark:text-gray-100 dark:bg-gray-900 antialiased">
         {{ $slot }}
     </div>
