7d0b0ae80a7fa87f8f008a5417b5a7baeedf6855
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-09-27 21:34:16 -0500

    creacion del Dropdown menu para cambio de idioma

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -47,6 +47,50 @@
                     <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
                         href="{{ route('home') }}">Home</a>
                 </div>
+                <div x-data="{ isOpen: true }" x-init="() => { isOpen = false }" class="relative inline-block mr-3">
+                    <!-- Dropdown toggle button -->
+                    <button @click="isOpen = !isOpen"
+                        class="relative z-10 block p-2 text-gray-700 bg-white border border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none">
+                        <i class="bi bi-globe"></i>
+                    </button>
+
+                    <!-- Dropdown menu -->
+                    <div x-show="isOpen" @click.away="isOpen = false"
+                        x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="opacity-0 scale-90" x-transition:enter-end="opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-100"
+                        x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-90"
+                        class="absolute right-0 z-20 w-48 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-700">
+
+                        <a href="#"
+                            class="flex items-center px-3 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <i class="bi bi-person-circle w-5 h-5 mx-1"></i>
+                            <span class="mx-1">
+                                view profile
+                            </span>
+                        </a>
+
+                        <hr class="border-gray-200 dark:border-gray-700 ">
+
+                        <a href="#"
+                            class="flex items-center p-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <i class="bi bi-building w-5 h-5 mx-1"></i>
+                            <span class="mx-1">
+                                Company profile
+                            </span>
+                        </a>
+
+                        <hr class="border-gray-200 dark:border-gray-700 ">
+
+                        <a href="#"
+                            class="flex items-center p-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <i class="bi bi-question-circle w-5 h-5 mx-1"></i>
+                            <span class="mx-1">
+                                Help
+                            </span>
+                        </a>
+                    </div>
+                </div>
 
                 @if (Route::has('login'))
                     <div class="flex justify-center md:block">
