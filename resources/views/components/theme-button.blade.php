299470a17d005a2c343c7f2af433baa4aa3a0d89
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2023-09-30 04:00:59 -0500

    creacion del componente theme button

---
+++ resources/views/components/theme-button.blade.php
@@ -0,0 +1,3 @@
+<button class="fixed bottom-4 right-4 p-2 px-3 rounded-full bg-gray-800 dark:bg-white text-white dark:text-black"
+    id="modoDarkBtn" style="z-index: 9999;"><i class="bi bi-moon-stars-fill text-xl"></i>
+</button>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,12 +15,9 @@
 </head>
 
 <body>
-    <button class="fixed bottom-4 right-4 p-2 px-3 rounded-full bg-gray-800 dark:bg-white text-white dark:text-black"
-        id="modoDarkBtn" style="z-index: 9999;"><i class="bi bi-moon-stars-fill text-xl"></i>
-    </button>
+    <x-theme-button/>
 
     <x-navbar-guest/>
-
 
     <div class="font-sans w-full mt-16  bg-gray-200 text-gray-900 dark:text-gray-100 dark:bg-gray-900 antialiased">
         {{ $slot }}
