8733a297b463df853ee8c87660b8f95a0cba4090
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-09-29 20:28:45 -0500

    creacion de la seccion de modulos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,20 +8,26 @@
 
             <span
                 class="text-transparent bg-gradient-to-tr bg-clip-text from-blue-500 via-pink-500 to-red-500 dark:from-sky-300 dark:via-pink-300 dark:to-red-500">
-                Rest
+                Rest V1
             </span>
         </h1>
 
-        <div class="container px-6 py-10 mx-auto">
+        <p class="max-w-3xl mx-auto mt-6 text-lg text-center text-gray-700 dark:text-white md:text-xl">
+            Explora y utiliza los diversos módulos de nuestra API REST para satisfacer tus necesidades de desarrollo de
+            manera eficiente y personalizada.
+        </p>
+
+        <section class="container px-6 py-10 mx-auto">
             <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
                 <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
                     <span class="inline-block text-blue-500 dark:text-blue-400">
                         <i class="bi bi-fire text-4xl"></i>
                     </span>
-                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">{{ __('Elegant Dark Mode')}}</h1>
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                        {{ __('Elegant Dark Mode') }}</h1>
 
                     <p class="text-gray-500 dark:text-gray-300">
-                        {{ __('TextCardElegantDarkMode')}}
+                        {{ __('TextCardElegantDarkMode') }}
                     </p>
                 </div>
 
@@ -30,10 +36,11 @@
                         <i class="bi bi-puzzle text-4xl"></i>
                     </span>
 
-                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">{{ __('Easy to use')}}</h1>
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">{{ __('Easy to use') }}
+                    </h1>
 
                     <p class="text-gray-500 dark:text-gray-300">
-                        {{ __('TextCardEasyToUse')}}
+                        {{ __('TextCardEasyToUse') }}
                     </p>
                 </div>
 
@@ -42,11 +49,12 @@
                         <i class="bi bi-stars text-4xl"></i>
                     </span>
 
-                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">{{ __('Simple and clean designs')}}
+                    <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                        {{ __('Simple and clean designs') }}
                     </h1>
 
                     <p class="text-gray-500 dark:text-gray-300">
-                        {{ __('TextCardSimpleAndCleanDesigns')}}
+                        {{ __('TextCardSimpleAndCleanDesigns') }}
                     </p>
                 </div>
 
@@ -56,54 +64,75 @@
                     </span>
 
                     <h1 class="text-xl font-semibold text-gray-700 capitalize dark:text-white">
-                        {{ __('Fully Responsive Components')}}
+                        {{ __('Fully Responsive Components') }}
                     </h1>
 
                     <p class="text-gray-500 dark:text-gray-300">
-                        {{ __('TextCardFullyResponsiveComponents')}}
+                        {{ __('TextCardFullyResponsiveComponents') }}
                     </p>
                 </div>
             </div>
-        </div>
+        </section>
 
-        <div class="container px-6 py-8 mx-auto">
-            <h2 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Version
-                #1</h2>
-
-            <div class="grid gap-8 mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                <div class="w-full max-w-xs text-center">
-                    <img class="object-cover object-center w-full h-48 mx-auto rounded-lg"
-                        src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80"
-                        alt="avatar" />
-
-                    <div class="mt-2">
-                        <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">Ahmed Omer</h3>
-                        <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">CEO</span>
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+                <div class="flex flex-wrap -m-4">
+                    <div class="p-4 md:w-1/3">
+                        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                            <img class="lg:h-48 md:h-36 w-full object-cover object-center"
+                                src="https://th.bing.com/th/id/OIP.ekWUiBouFunNqguGwNh5uwHaEk?pid=ImgDet&rs=1"
+                                alt="blog">
+                            <div class="p-6">
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3 dark:text-white">Products
+                                </h1>
+                                <p class="leading-relaxed mb-3">Explora todos los endpoints del modulo de productos</p>
+                                <div class="flex items-center flex-wrap ">
+                                    <a href="#"
+                                        class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">View Docs
+                                        <i class="bi bi-arrow-right text-2xl ml-2"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div class="w-full max-w-xs text-center">
-                    <img class="object-cover object-center w-full h-48 mx-auto rounded-lg"
-                        src="https://images.unsplash.com/photo-1516756587022-7891ad56a8cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-                        alt="avatar" />
-
-                    <div class="mt-2">
-                        <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">Jane Doe</h3>
-                        <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">Co-founder</span>
+                    <div class="p-4 md:w-1/3">
+                        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                            <img class="lg:h-48 md:h-36 w-full object-cover object-center"
+                                src="https://www.nobbot.com/wp-content/uploads/2017/04/etiqueta-categoria-920x515.jpg"
+                                alt="blog">
+                            <div class="p-6">
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3 dark:text-white">Categorias
+                                </h1>
+                                <p class="leading-relaxed mb-3">Explora todos los endpoints del modulo de productos</p>
+                                <div class="flex items-center flex-wrap ">
+                                    <a href="#"
+                                        class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">View Docs
+                                        <i class="bi bi-arrow-right text-2xl ml-2"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div class="w-full max-w-xs text-center">
-                    <img class="object-cover object-center w-full h-48 mx-auto rounded-lg"
-                        src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80"
-                        alt="avatar" />
-
-                    <div class="mt-2">
-                        <h3 class="text-lg font-medium text-gray-700 dark:text-gray-200">Steve Ben</h3>
-                        <span class="mt-1 font-medium text-gray-600 dark:text-gray-300">UI/UX</span>
+                    <div class="p-4 md:w-1/3">
+                        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                            <img class="lg:h-48 md:h-36 w-full object-cover object-center"
+                                src="https://www.contpaqi.com/Thu%20Mar%2016%202023-3.png"
+                                alt="blog">
+                            <div class="p-6">
+                                <h1 class="title-font text-lg font-medium text-gray-900 mb-3 dark:text-white">Usuarios
+                                </h1>
+                                <p class="leading-relaxed mb-3">Explora todos los endpoints del modulo de productos</p>
+                                <div class="flex items-center flex-wrap ">
+                                    <a href="#"
+                                        class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">View Docs
+                                        <i class="bi bi-arrow-right text-2xl ml-2"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
 </x-guest-layout>
