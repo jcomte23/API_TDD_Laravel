25857babcd8ae5c24b6245b61556bde8f04a1b46
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2023-09-30 03:55:22 -0500

    creacion del componente footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="bg-gray-200 dark:bg-gray-900">
+    <div class="container px-6 py-8 mx-auto">
+        <div class="flex flex-col items-center text-center">
+            <a href="https://javiercombita.com" target="_black">
+                <img id="marca" class="w-auto h-24" alt="marca">
+            </a>
+        </div>
+
+        <hr class="my-6 border-black md:my-10 dark:border-gray-700" />
+
+        <div class="flex flex-col items-center sm:flex-row sm:justify-between">
+            <p class="text-sm dark:text-gray-300">© Copyright 2023 Javier Cómbita Téllez.
+                {{ __('All Rights Reserved') }}</p>
+
+            <div class="flex -mx-2">
+                <a href="https://twitter.com/jcomte23" target="_blank"
+                    class="mx-2 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                    aria-label="Reddit">
+                    <i class="bi bi-twitter-x text-2xl"></i>
+                </a>
+
+                <a href="https://co.linkedin.com/in/javier-c%C3%B3mbita-t%C3%A9llez-4b4aa3258" target="_blank"
+                    class="mx-2 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                    aria-label="Linkedin">
+                    <i class="bi bi-linkedin text-2xl"></i>
+                </a>
+
+                <a href="https://github.com/jcomte23" target="_blank"
+                    class="mx-2 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                    aria-label="Github">
+                    <i class="bi bi-github text-2xl"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
